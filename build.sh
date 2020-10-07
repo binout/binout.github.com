857d2f8c1f830326349be80fc2f5de5da5aa2aad
binout <benoit.prioux@gmail.com>
2020-10-07 21:30:59 +0200

    update resume

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 docker run --rm -v $(pwd):/documents/ asciidoctor/docker-asciidoctor asciidoctor *.adoc
+mv README.html index.html
 docker run --rm -v $(pwd):/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf resume.adoc
